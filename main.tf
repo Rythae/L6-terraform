94cdd5503a1ee546ac3102ea0fe2e380e0518fc6
rythae <auxano42@gmail.com>
2021-04-18 18:10:14 +0100

    single web server

--- main.tf
+++ main.tf
@@ -10,3 +10,45 @@ resource "aws_instance" "testing-terraform" {
     Name = "HelloTerraform"
   }
 }
+
+resource "aws_instance" "testing-terraform-2" {
+  ami           = "ami-013f17f36f8b1fefb"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "HelloTerraform-2"
+  }
+}
+
+
+resource "aws_instance" "testing-terraform-3" {
+  ami           = "ami-013f17f36f8b1fefb"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "HelloTerraform-3"
+  }
+}
+
+resource "aws_instance" "web-server-instance" {
+  ami           = "ami-013f17f36f8b1fefb"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_instance.id]
+  
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  tags = {
+    Name = "terraform-web-server"
+  }
+}
+
+resource "aws_security_group" "allow_instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
